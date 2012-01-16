d6ece0bf09e17f0ec51e92c6eb669a53d375f1e4
Mikay Gao <imikay@gmail.com>
2012-01-16 20:35:48 +0800

    Added find command

--- site.cmd
+++ site.cmd
@@ -1,2 +1,14 @@
 @echo off
+
+IF "%1" == "find" goto list
+
+IF NOT "%s" == "find" goto normal
+goto end
+
+:normal
 php.exe "E:\php-commands\site.php" %*
+
+:list
+php.exe "E:\php-commands\site.php" list|find "%2"
+
+:end
