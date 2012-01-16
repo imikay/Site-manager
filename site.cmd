ef61f19037a447c277c3e52efcf22781560d01ee
Mikay Gao <imikay@gmail.com>
2012-01-16 21:14:08 +0800

    Added 'open' command through windows shell

--- site.cmd
+++ site.cmd
@@ -1,14 +1,23 @@
 @echo off
 
 IF "%1" == "find" goto list
+IF "%1" == "open" goto open
 
-IF NOT "%s" == "find" goto normal
+IF NOT "%1" == "find" goto normal
+
 goto end
 
 :normal
 php.exe "E:\php-commands\site.php" %*
+goto end
 
 :list
 php.exe "E:\php-commands\site.php" list|find "%2"
+goto end
+
+:open
+explorer.exe "http://%2"
+echo dasfadsf
+goto end
 
 :end
